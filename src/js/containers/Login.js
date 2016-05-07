36d19abbfa3fbf2a1a8d7cf95afda3752ee3f6ce
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-05-07 23:13:07 +1200

    Add different login

--- src/js/containers/Login.js
+++ src/js/containers/Login.js
@@ -1,11 +1,34 @@
-import React from 'react';
+// import React from 'react';
+//
+// export default class Login extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Login</h1>
+//       </div>
+//     );
+//   }
+// }
+import React, { Component } from 'react';
 
-export default class Login extends React.Component {
+export default class Login extends Component {
   render() {
-    return (
+    const { errorMessage } = this.props;
+    return {
       <div>
-        <h1>Login</h1>
+        <Form onSubmit={ ( formData ) => this.handleSubmit( formData ) } >
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </Form>
       </div>
-    );
+    }
+  }
+
+  handleSubmit( formData ) {
+    const { username, password } = this.this.props;
+    const { login } = this.props;
+    login( username, password );
   }
 }
