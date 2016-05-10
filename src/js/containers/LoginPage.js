f5d86958d643bed831380367caf748c75f4a84be
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-05-10 22:42:33 +1200

    Add todo

--- src/js/containers/LoginPage.js
+++ src/js/containers/LoginPage.js
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import { login } from 'reducers/authentication';
 import LoginForm from 'components/LoginForm';
 
+// TODO: understand where login comes from
+
 function mapStateToProps( state ) {
   return {
     errorMessage: state.authentication.errorMessage
