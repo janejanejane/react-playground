aa365907a975c63c3406c91beef11f64aebaefa5
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-05-12 23:17:08 +1200

    Remove todo

--- src/js/containers/LoginPage.js
+++ src/js/containers/LoginPage.js
@@ -2,8 +2,6 @@ import { connect } from 'react-redux';
 import { login } from 'reducers/authentication';
 import LoginForm from 'components/LoginForm';
 
-// TODO: understand where login comes from
-
 function mapStateToProps( state ) {
   return {
     errorMessage: state.authentication.errorMessage
