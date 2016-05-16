8781818b3041516a2c334a8cd2c0c09f3cc55d4b
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-05-16 22:45:32 +1200

    Add stateless form

---
+++ src/js/components/SignupForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Signup extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Signup</h1>
+        Username: <input type="text" />
+        Password: <input type="text" />
+      </div>
+    );
+  }
+}
