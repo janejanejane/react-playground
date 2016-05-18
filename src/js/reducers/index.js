64a756c358d82f80e24dacb4493a6072e15fbc41
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-05-18 23:33:03 +1200

    Add registration reducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,11 @@
 import { routerReducer as routing } from 'react-router-redux'
 import { combineReducers } from 'redux'
 import authentication from './authentication'
+import registration from './registration'
 
 const rootReducer = combineReducers({
   authentication,
+  registration,
   routing
 })
 
