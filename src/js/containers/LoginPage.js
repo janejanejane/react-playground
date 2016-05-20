716775ccea60be4b67cb7e0f4a72b9a85a9bfa26
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-05-20 23:17:42 +1200

    Add placedolder

--- src/js/containers/LoginPage.js
+++ src/js/containers/LoginPage.js
@@ -2,6 +2,10 @@ import { connect } from 'react-redux';
 import { login } from 'reducers/authentication';
 import LoginForm from 'components/LoginForm';
 
+function handleSubmit() {
+  console.log( 'handleSubmit' )
+}
+
 function mapStateToProps( state ) {
   return {
     errorMessage: state.authentication.errorMessage
