911fd199b0b35bfe8a86ee3373c00e06c3ff1cc1
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2016-08-01 23:13:14 +1200

    Add json get

--- public/localize.js
+++ public/localize.js
@@ -1,3 +1,10 @@
 var p = new Polyglot( { phrases: { "hello": "Hello!" } } );
 
 console.log( p.t( 'hello' ) );
+
+$.get( 'phrases.json', function( result ) {
+    console.log( 'result:', result );
+
+    var p2 = new Polyglot( { phrases: result } );
+    console.log( p2.t( 'hello' ) );
+});
